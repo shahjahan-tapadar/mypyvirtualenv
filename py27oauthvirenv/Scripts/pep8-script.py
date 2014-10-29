d2014e6a3375f501055b78f42f1cb145e13d16dd
najeebt <najeebt@rapidvaluesolutions.com>
2014-10-29 20:07:28 +0530

    latest env

---
+++ py27oauthvirenv/Scripts/pep8-script.py
@@ -0,0 +1,10 @@
+#!C:\Users\NajeebT\py27oauthvirenv\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'pep8==1.5.7','console_scripts','pep8'
+__requires__ = 'pep8==1.5.7'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('pep8==1.5.7', 'console_scripts', 'pep8')()
+    )
